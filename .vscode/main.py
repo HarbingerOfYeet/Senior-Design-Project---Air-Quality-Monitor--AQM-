abf74a0c5b9f4ca1da9382eeba3f8d32ca198d8c
HarbingerOfYeet <winston.ngo.den@gmail.com>
2023-09-29 11:13:54 -0400

    Added LCD files

--- .vscode/main.py
+++ .vscode/main.py
@@ -1,13 +1,2 @@
 from machine import Pin
 from utime import sleep
-
-speaker = Pin(16, Pin.OUT)
-led = Pin("LED", Pin.OUT)
-
-print("Speaker turning on...")
-
-while True:
-    speaker.toggle()
-    #led.toggle()
-    #sleep(0.5)
-    #speaker.low()
---
+++ .vscode/test.py
@@ -0,0 +1,21 @@
+import LCD1602
+import time
+
+lcd = LCD1602.LCD1602(16, 2)
+
+try:
+    while True:
+        # set the cursor to column 0, line 1
+        lcd.setCursor(0, 0)
+
+        lcd.printout("Waveshare")
+
+        lcd.setCursor(0, 1)
+
+        lcd.printout("Hello World!")
+        time.sleep(0.1)
+
+        print("Running")
+except(KeyboardInterrupt):
+    lcd.clear()
+    del lcd
