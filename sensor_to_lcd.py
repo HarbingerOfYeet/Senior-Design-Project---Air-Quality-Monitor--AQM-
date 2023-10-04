e2ff8b4c24e62a30d47aeb1dea75569882d6aa47
HarbingerOfYeet <winston.ngo.den@gmail.com>
2023-10-04 18:30:21 -0400

    Sensor to LCD working

--- sensor_to_lcd.py
+++ sensor_to_lcd.py
@@ -7,6 +7,7 @@ lcd = LCD1602.LCD1602(16, 2)
 led = Pin("LED", Pin.OUT)
 pms = HPMA.HPMA(0)
 
+# boot up sensor
 print("resetting sensor...")
 pms.flush()
 pms.stopMeasurement()
@@ -18,30 +19,40 @@ pms.startMeasurement()
 pms.stopAutoSend()
 
 for i in range(15): # throw away first measurements because of internal running average over 10s and fan speed up
+    
+    # read sensor data
     output_string = pms.readMeasurement()
     [pm25, pm10] = str(output_string).split(' ')
 
+    # output sensor data to LCD
     lcd.setCursor(0, 0)
-    lcd.printout('PM2.5: {0}'.format(pm25))
+    lcd.printout('PM2.5:{0} 10:{1}'.format(pm25, pm10))
 
     lcd.setCursor(0, 1)
-    lcd.printout('PM10: {0}'.format(pm10))
+    lcd.printout("Measurement {0}".format(i))
 
     sleep(1)
 
 # output real data
 print("Starting data collection...")
+lcd.clear()
 for i in range(15):
+
+    # read sensor data
     output_string = pms.readMeasurement()
     [pm25, pm10] = str(output_string).split(' ')
 
+    # output sensor data to LCD
     lcd.setCursor(0, 0)
-    lcd.printout('PM2.5: {0}'.format(pm25))
+    lcd.printout('PM2.5:{0} 10:{1}'.format(pm25, pm10))
 
     lcd.setCursor(0, 1)
-    lcd.printout('PM10: {0}'.format(pm10))
+    lcd.printout("Measurement {0}".format(i))
     
     sleep(1)
 
-print("powering down")
+lcd.clear()
+lcd.setCursor(0, 0)
+lcd.printout("power down")
+lcd.clear()
 pms.stopMeasurement()
